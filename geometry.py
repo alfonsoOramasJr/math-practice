a9a2c42ce16db11b7de9c087b7259b74e7884976
workordeath <alfonso.oramas@icloud.com>
2024-02-29 23:09:17 -0500

    NEW: Established a way to check if the user is correct by simply adding to the counter, the counter is always displayed above the problem.

--- geometry.py
+++ geometry.py
@@ -12,7 +12,16 @@ class Problems:
         self.displayCurrentScore()
         self.perimeterOfASquare()
 
+    def setAnswer(self):
+        self.user_answer = input(">> ")
+        self.user_answer = int(self.user_answer)
+    
+    def checkIfUserIsCorrect(self):
+        if (self.user_answer == self.correct_answer):
+            self.correct_answers += 1
 
+    def displayCurrentScore(self):
+        print(f"\t\t\tcorrect answers [{self.correct_answers}/{self.cycles}]")
 
     def perimeterOfASquare(self):
         side_length = random.randint(0, 24)
