21ebb05f116a20db41a69ffaf7ad3e37996f31fd
workordeath <alfonso.oramas@icloud.com>
2024-02-29 22:45:50 -0500

    NEW: Defined basic functions for getting a question/answer from our program.

---
+++ geometry.py
@@ -0,0 +1,14 @@
+class Problems:
+    def __init__(self):
+        pass
+
+    def getQuestion(self):
+        pass
+    def getAnswer(self):
+        pass
+
+def main():
+    problem = Problems()
+    for problems in range(100):
+        problem.getQuestion()
+        problem.getAnswer()
